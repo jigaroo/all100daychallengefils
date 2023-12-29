0e6f4f78d139733be5a88b1174641b28c013d6a4
jigaroo <clashftar@gmail.com>
2023-12-29 19:02:34 +0330

    day47

---
+++ day37/exercise.js
@@ -0,0 +1,44 @@
+// Practice what you learned!
+
+// 1) Select the two <button> elements and store them in two different variables.
+//    - Select the first button without adding or using any "id"
+//    - Select the second button by using an "id"
+
+const firstbuttonElement = document.querySelector('button');
+const secondbuttonElement = document.getElementById('change-btn')
+
+// 2) Add "click" event listener to both buttons (with two different functions).
+//    The functions should "console.dir()" the clicked buttons.
+//    - Output the first button by using the variable in which it's stored
+//    - Output the second button WITHOUT using the variable in which it's stored
+// 3) Now select and store the paragraphs mentioned in the text you see on the page
+//    (first and third paragraph)
+//    - Select BOTH paragraphs by drilling into the document and "navigating" to the
+//      mentioned elements
+//    - If you struggle with DOM drilling, use "ids" instead but watch the solution!
+
+const firstparaghraphElement =document.body.children[2].children[1];
+const thirdparagrapgElement =document.body.children[2].children[3];
+//const thirdparagrapgElement =firstparaghraphElement.nextElementSibling.nextElementSibling;
+
+
+
+// 4) Change the functions from (2) such that:
+//    - The first button removes the third paragraph (i.e. the <p> prior to it)
+//    - The second button changes the background color of the first paragraph to blue
+// 5) Solve (4) both by changing the "inline styles" as well as by adding CSS classes
+//    Note: You'll have to add those classes to the styles.css file first!
+function removeParaghraph(){
+    console.dir(firstbuttonElement)
+    thirdparagrapgElement.remove();
+   }
+   function changeBackgroundColor(event){
+       console.dir(event.target)
+       //firstparaghraphElement.style.backgroundColor ='blue';
+      // firstparaghraphElement.className = 'blue-bg';
+      firstparaghraphElement.classList.add('blue-bg')
+   }
+   
+   firstbuttonElement.addEventListener('click',removeParaghraph);
+   secondbuttonElement.addEventListener('click',changeBackgroundColor);
+   
