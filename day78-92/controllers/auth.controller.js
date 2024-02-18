c393a5f3d21c3752b5876fe8efe114d2ac35fafa
jigaroo <clashftar@gmail.com>
2024-02-18 11:20:43 +0330

    end of day 80 connect to data base

--- day78-92/controllers/auth.controller.js
+++ day78-92/controllers/auth.controller.js
@@ -2,11 +2,16 @@ function getSingnup(req,res){
     res.render('customer/auth/signup');
 }
 
+function signup(req,res){
+    
+}
+
 function getLogin(req,res){
     //
 }
 
 module.exports = {
     getSingnup:getSingnup,
-    getLogin:getLogin
+    getLogin:getLogin,
+    signup:signup
 };
